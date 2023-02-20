4763b2013527c700f8b8bea0a55c16ceecab87fc
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-02-20 16:37:33 -0400

    add inputs and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
+import formLogo from "./assets/form-logo.png";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(userData) {
+    console.log(userData);
+  }
+
+  console.log(errors);
   return (
-    <div className="App">
-      <h2>Hello World!</h2>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h2></h2>
+      <img src={formLogo} alt="imagem-logo-form" />
+
+      <label>
+        Nome:
+        <input type="text" {...register("name", { required: true })} />
+        {errors.name && <span>O nome é obrigatório!</span>}
+      </label>
+      <label>
+        Email:
+        <input type="email" {...register("email")} />
+      </label>
+      <label>
+        Senha:
+        <input type="password" {...register("password")} />
+      </label>
+      <label>
+        Confirmar senha:
+        <input type="password" {...register("Confirmpassword ")} />
+      </label>
+
+      <button type="submit">Cadastrar-se </button>
+    </form>
   );
 }
 
